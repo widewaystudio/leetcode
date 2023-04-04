f31fe321baeb2f89b6d0b88981a0a27d4ad5ddb0
widewaystudio <woshixixin@126.com>
2023-04-04 15:33:14 +0800

    find word in string

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-function delNum(Arr,target){
-    if(Object.prototype.toString.call(Arr) !== '[object Array]') return 0;
-    let r = new RegExp(target,'g');
-    let temps = Arr.join(',').replace(r,'');
-    r = /\d+/g;
-    return temps.match(r).length;
-}
-let num1 = [0,1,2,2,3,0,4,2];
-let val = 2;
 
-console.log(delNum(num1,val));
+function substring(str,needle){
+    let reg = new RegExp(needle);
+    let r = reg.exec(str);
+    return r ? r.index : -1 ;
+}
+console.log(substring('hello','ll'))
