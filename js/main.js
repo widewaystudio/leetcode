51cf4188a17fd1e2c0f95692f97a3c844d9f42bb
widewaystudio <woshixixin@126.com>
2023-04-04 21:28:01 +0800

    find word in string

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
+ function twoN(Arr,t){
+    let temp = [];
+    if(Object.prototype.toString.call(Arr) !== '[object Array]' || Arr.length < 2 ) return temp;
+    let l = Arr.length;
+    let obj = {};
+    for(let i = 0; i < l; i++){
+        let a = t - Arr[i];
+        if(typeof obj[a] === 'number'){
+            return temp.concat(obj[a],i);
+        }else{
+            obj[Arr[i]] = i;
+        }
 
-function substring(str,needle){
-    let reg = new RegExp(needle);
-    let r = reg.exec(str);
-    return r ? r.index : -1 ;
-}
-console.log(substring('hello','ll'))
+    }
+
+    return temp;
+
+ }
+
+ console.log(twoN([2,7,11,15],9))
