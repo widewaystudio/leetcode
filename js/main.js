80488f359f3b356ef78855a3edeadb2be92eb1d5
widewaystudio <woshixixin@126.com>
2023-04-04 15:25:51 +0800

    dels_number

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-function duplicate(Arr){
+function delNum(Arr,target){
     if(Object.prototype.toString.call(Arr) !== '[object Array]') return 0;
-    let reg = /(\d).*\1/g;
-    let r = /\d+/g;
-    let s = Arr.join(',').replace(reg,'$1');
-    console.log(s);
-    return s.match(r).length;
+    let r = new RegExp(target,'g');
+    let temps = Arr.join(',').replace(r,'');
+    r = /\d+/g;
+    return temps.match(r).length;
 }
+let num1 = [0,1,2,2,3,0,4,2];
+let val = 2;
 
-console.log(duplicate([0,0,1,1,1,2,2,3,3,4]));
+console.log(delNum(num1,val));
